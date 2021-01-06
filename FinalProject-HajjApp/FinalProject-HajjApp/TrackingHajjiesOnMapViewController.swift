87cec945424b0283412e0a8d1bc91d31eac0aca8
NouraAlthenayan <noura.yarob@gmail.com>
2021-01-06 17:37:24 +0300

    Successfully fetched hajj's coordinates based in search bar text, and displayed it on map as a pin

--- FinalProject-HajjApp/FinalProject-HajjApp/TrackingHajjiesOnMapViewController.swift
+++ FinalProject-HajjApp/FinalProject-HajjApp/TrackingHajjiesOnMapViewController.swift
@@ -18,50 +18,43 @@ class TrackingHajjiesOnMapViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         mapView.delegate = self
-        fetchHajjCoordinates()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        fetchHajjCoordinates()
 
-    }
     
     func searchFor(text: String) {
-        
-//        apiService.search(tag: text, success: { results in
-//
-//                print("Here are you pictures:\(results)")
-//                DispatchQueue.main.async {
-//                    self.showPinsOnMap(pins: results)
-//                }
-//
-//            }) { error in
-//
-//            DispatchQueue.main.async {
-//                // Show Alert when no photos found
-//                let alert = UIAlertController(title: "No Photos Found",
-//                                              message: "Could not find any photo with specified tag. Try another search query",
-//                                              preferredStyle: UIAlertController.Style.alert)
-//
-//                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//                self.present(alert, animated: true, completion: nil)
-//
-//                print("No Photos Found..")
-//
-//            }
-//            switch error {
-//
-//            case FlickrSearchAPI.APIError.noPhotosFound:
-//                break
-//
-//            default:
-//                break
-//            }
-        }
+        //"H42-C1-001"
 
+        FirebaseConstants.users.whereField("permitNumber", isEqualTo: text)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("hhhh \(document.documentID) => \(document.data())")
+//                        print(document.data().index(forKey: "currentLocation"))
+                        let getcurrentLocation = document["currentLocation"] as! GeoPoint
+                        print(getcurrentLocation.latitude)
+                        print(getcurrentLocation.longitude)
+                        
+                        //getting current location
+                        let location = CLLocationCoordinate2DMake(getcurrentLocation.latitude, getcurrentLocation.longitude)
+                        print("hajj's location: \(location)")
+                        let region = MKCoordinateRegion(center: location, latitudinalMeters: 500.0, longitudinalMeters: 700.0)
+                        self.mapView.setRegion(region, animated: true)
+                        // Drop a pin
+                        let dropPin = MKPointAnnotation()
+                        dropPin.coordinate = location
+                        dropPin.title = "موقع الحاج -رقم تصريح كذت-"
+                        self.mapView.addAnnotation(dropPin)
+                        
+                    }
+                }
+            }
+        
+        
+    }
+    
     // Showing pin on map
     func showPinsOnMap(pins: [MKAnnotation]) {
 
@@ -69,32 +62,32 @@ class TrackingHajjiesOnMapViewController: UIViewController, MKMapViewDelegate {
         mapView.showAnnotations(pins, animated: true)
     }
     
-    func fetchHajjCoordinates() {
-        print("function")
-        let userId = CrewQRCodeScannerViewController.uidFromQRCode
-        FirebaseConstants.users.getDocuments { (snapshot, err) in
-            if let err = err {
-                print("Error getting user's name: \(err)")
-            } else {
-                print("fffff")
-
-                if let currentUserDoc = snapshot?.documents.first(where: { ($0["uid"] as? String) == userId }) {
-                    print("kkkkkk")
-
-                    let getcurrentLocation = currentUserDoc["currentLocation"] as! GeoPoint
-                    let location = CLLocationCoordinate2DMake(getcurrentLocation.latitude, getcurrentLocation.longitude)
-                    print("hajj's location: \(location)")
-                    let region = MKCoordinateRegion(center: location, latitudinalMeters: 500.0, longitudinalMeters: 700.0)
-                    self.mapView.setRegion(region, animated: true)
-                    // Drop a pin
-                    let dropPin = MKPointAnnotation()
-                    dropPin.coordinate = location
-                    dropPin.title = "موقع الحاج -رقم تصريح كذت-"
-                    self.mapView.addAnnotation(dropPin)
-                }
-            }
-        }
-    }
+//    func fetchHajjCoordinates() {
+//        print("function")
+//        let userId = CrewQRCodeScannerViewController.uidFromQRCode
+//        FirebaseConstants.users.getDocuments { (snapshot, err) in
+//            if let err = err {
+//                print("Error getting user's name: \(err)")
+//            } else {
+//                print("fffff")
+//
+//                if let currentUserDoc = snapshot?.documents.first(where: { ($0["uid"] as? String) == userId }) {
+//                    print("kkkkkk")
+//
+//                    let getcurrentLocation = currentUserDoc["currentLocation"] as! GeoPoint
+//                    let location = CLLocationCoordinate2DMake(getcurrentLocation.latitude, getcurrentLocation.longitude)
+//                    print("hajj's location: \(location)")
+//                    let region = MKCoordinateRegion(center: location, latitudinalMeters: 500.0, longitudinalMeters: 700.0)
+//                    self.mapView.setRegion(region, animated: true)
+//                    // Drop a pin
+//                    let dropPin = MKPointAnnotation()
+//                    dropPin.coordinate = location
+//                    dropPin.title = "موقع الحاج -رقم تصريح كذت-"
+//                    self.mapView.addAnnotation(dropPin)
+//                }
+//            }
+//        }
+//    }
 }
 
 //MARK: - Search Bar Delegate
@@ -106,9 +99,10 @@ extension TrackingHajjiesOnMapViewController: UISearchBarDelegate {
               search.isEmpty == false else {
             return
         }
-        
+        print(search)
         searchBar.resignFirstResponder()
         searchFor(text: search)
+        print("hh")
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
